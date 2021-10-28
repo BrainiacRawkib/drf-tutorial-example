1826acd006439a2f4de3825c1e64655d3e4cd60f
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-10-28 04:37:21 -0700

    :hammer: Relationships and Hyperlinked APIs

--- snippets_src/drfsnippets/settings.py
+++ snippets_src/drfsnippets/settings.py
@@ -119,6 +119,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+# REST_FRAMEWORK settings
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- snippets_src/snippets/urls.py
+++ snippets_src/snippets/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 
-urlpatterns = [
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-    path('snippets/', SnippetList.as_view()),
-    path('snippets/<int:pk>/', SnippetDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+# API Endpoints
+urlpatterns = format_suffix_patterns([
+    path('', api_root),
+    path('snippets/', SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/<highlight>/', SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+])
